491754aacc000374394d50b468b5ea4099e624c2
Emmett Coin <emmett@ejtalk.com>
2023-03-07 10:43:23 -0500

    firstLoadToVSCejc

--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from utils.dialog_event import DialogPacket as DialogPacket
-from utils.dialog_event import DialogEvent as DialogEvent
-from utils.dialog_event import Feature as Feature
-from utils.dialog_event import TextFeature as TextFeature
-from utils.dialog_event import Token as Token
