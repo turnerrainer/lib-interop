13e5d5de643fc79ccbcab78cc855f9a667acefec
David Attwater <david.attwater@talkmap.com>
2023-07-21 23:18:54 +0100

    Partial implementation of  dialog_event code

--- javascript/lib/dialog_event.js
+++ javascript/lib/dialog_event.js
@@ -2,25 +2,72 @@ const fs = require('fs');
 // const yaml = require('js-yaml');
 
 class DialogPacket {
-    constructor() {
-        this.packet={}
+    constructor(packet) {
+        if (packet!=undefined) { 
+            Object.assign(this,packet); 
+        }
     }
 
-    // Load the packet from a string
+    // Parse the JSON packet into elements of this class.
     load_json(s) {
-        this.packet=JSON.parse(s)
+        Object.assign(this,JSON.parse(s));
     }
 
+    //Print the packet as a JSON string, pass through prettify arguments.
     dump_json() {
-        return JSON.stringify(this.packet)
+        //return JSON.stringify(this,arguments[0],arguments[1]);
+        return JSON.stringify(this,null,4);
+    }
+}
+
+class Span extends DialogPacket {
+    constructor( {end_offset_msec=undefined,start_offset_msec=undefined} = {}) {
+        if (end_offset_msec!=undefined) { 
+            arguments[0].end_offset="PT"+(end_offset_msec/1000).toFixed(6);
+            arguments[0].end_offset_msec=undefined
+        }
+        if (start_offset_msec!=undefined) { 
+            arguments[0].start_offset="PT"+(start_offset_msec/1000).toFixed(6);
+            arguments[0].start_offset_msec=undefined
+        }
+        super(arguments[0]);
+    }
+}
+
+class DialogEvent extends DialogPacket {
+    constructor() {
+        super(arguments[0]);
+        if (this.features==undefined) {
+            this.features={};
+        }
     }
 
-    // load_yml(s) {
-    //    this.packet=yaml.load(s);
-    // }
-    // dump_yml() {
-    //     return yaml.safeDump(data);
-    //}
+    add_feature(feature_name,f) {
+        this.features[feature_name]=f
+        //Object.assign(this.features,f); 
+    }
+}
+
+class Feature extends DialogPacket {
+    constructor({mime_type=undefined,lang=undefined,encoding=undefined}={}) {
+        super(arguments[0]);
+        if (this.tokens==undefined) {
+            this.tokens=[];
+        }
+    }
+
+    add_token(t) {
+        this.tokens.push(t)
+    }
+}
+
+class Token extends DialogPacket {
+    constructor() {
+        super(arguments[0]);
+    }
 }
 
 this.DialogPacket=DialogPacket
+this.Span=Span
+this.DialogEvent=DialogEvent
+this.Feature=Feature
